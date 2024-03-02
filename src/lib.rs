d792a70c2b967c6ba065b0740bf5ed2b76bbe0dd
Andrii Kuchmenda <46228765+TRI99ERED@users.noreply.github.com>
2024-03-02 22:36:35 +0200

    finished logic and testing

--- src/lib.rs
+++ src/lib.rs
@@ -7,90 +7,90 @@ pub use fs_scout_macros::*;
 #[macro_export]
 macro_rules! create_file {
     ($path:literal!) => {{
-        std::fs::File::create(fs_scout::valid_file!($path))
+        std::fs::File::create($crate::valid_file!($path))
             .expect("should be able to create a file")
     }};
     ($path:literal) => {{
-        std::fs::File::create(fs_scout::valid_file!($path))
+        std::fs::File::create($crate::valid_file!($path))
     }};
 }
 
 #[macro_export]
 macro_rules! create_new_file {
     ($path:literal!) => {{
-        fs_scout::not_exists!($path);
+        $crate::not_exists!($path);
         std::fs::File::options()
             .read(true)
             .write(true)
             .create_new(true)
-            .open(fs_scout::valid_file!($path))
+            .open($crate::valid_file!($path))
             .expect("should be able to create a new file")
     }};
     ($path:literal) => {{
-        fs_scout::not_exists!($path);
+        $crate::not_exists!($path);
         std::fs::File::options()
             .read(true)
             .write(true)
             .create_new(true)
-            .open(fs_scout::valid_file!($path))
+            .open($crate::valid_file!($path))
     }};
 }
 
 #[macro_export]
 macro_rules! open_file {
     ($path:literal!) => {{
-        fs_scout::exists!($path);
-        std::fs::File::open(fs_scout::valid_file!($path)).expect("should be able to open a file")
+        $crate::exists!($path);
+        std::fs::File::open($crate::valid_file!($path)).expect("should be able to open a file")
     }};
     ($path:literal) => {{
-        fs_scout::exists!($path);
-        std::fs::File::open(fs_scout::valid_file!($path))
+        $crate::exists!($path);
+        std::fs::File::open($crate::valid_file!($path))
     }};
 }
 
 #[macro_export]
 macro_rules! read_file {
     ($path:literal!) => {{
-        fs_scout::exists!($path);
-        &std::fs::read(fs_scout::valid_file!($path)).expect("should be able to read a file")
+        $crate::exists!($path);
+        &std::fs::read($crate::valid_file!($path)).expect("should be able to read a file")
     }};
     ($path:literal) => {{
-        fs_scout::exists!($path);
-        &std::fs::read(fs_scout::valid_file!($path))
+        $crate::exists!($path);
+        &std::fs::read($crate::valid_file!($path))
     }};
 }
 
 #[macro_export]
 macro_rules! write_file {
     ($path:literal!, $contents:expr) => {{
-        std::fs::write(fs_scout::valid_file!($path), $contents)
+        std::fs::write($crate::valid_file!($path), $contents)
             .expect("should be a ble to write to a file")
     }};
     ($path:literal, $contents:expr) => {{
-        std::fs::write(fs_scout::valid_file!($path), $contents)
+        std::fs::write($crate::valid_file!($path), $contents)
     }};
 }
 
 #[macro_export]
 macro_rules! create_dir {
     ($path:literal!) => {{
-        fs_scout::not_exists!($path);
-        std::fs::create_dir(fs_scout::valid_dir!($path))
+        $crate::not_exists!($path);
+        std::fs::create_dir($crate::valid_dir!($path))
             .expect("should be able to create a directory")
     }};
     ($path:literal) => {{
-        fs_scout::not_exists!($path);
-        std::fs::create_dir(fs_scout::valid_dir!($path))
+        $crate::not_exists!($path);
+        std::fs::create_dir($crate::valid_dir!($path))
     }};
 }
 
 #[macro_export]
 macro_rules! create_dir_all {
     ($path:literal!) => {{
-        std::fs::create_dir_all(fs_scout::valid_dir_all!($path))
+        std::fs::create_dir_all($crate::valid_dir_all!($path))
             .expect("should be able to create all directories")
     }};
     ($path:literal) => {{
-        std::fs::create_dir_all(fs_scout::valid_dir_all!($path))
+        std::fs::create_dir_all($crate::valid_dir_all!($path))
     }};
 }
--- tests/specs.rs
+++ tests/specs.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
 
-use std::fs::create_dir;
+use std::fs::{create_dir, read};
 
 use fs_scout::*;
 
@@ -14,30 +14,49 @@ fn example() {
     // let reserved_char = valid_dir!("?");
     // let nul_char = valid_dir!("\0");
     // let control_char = valid_dir!("\n");
-    // let trailing_dot = valid_dir!("c.");
-    // let trailing_space = valid_dir!("c ");
-    let hidden = valid_dir!(".c");
-    let disk_abs_b = valid_file!("C:\\Letter.txt");
-    let disk_abs_f = valid_file!("C:/Letter.txt");
-    let abs_b = valid_file!("\\Letter.txt");
-    let abs_f = valid_file!("/Letter.txt");
-    let unc_d_disk = valid_dir!("\\\\?\\C:\\user\\");
-    let cur_b = valid_dir!(".\\inthisdir");
-    let cur_f = valid_dir!("./inthisdir");
-    let parent_b = valid_dir!("..\\..\\greatgrandparent");
-    let parent_f = valid_dir!("../../greatgrandparent");
+    // let trailing_dot = valid_file!("any.");
+    // let trailing_space = valid_dir!("any ");
+
+    let abs_file_b = valid_file!("\\Letter.txt");
+    let abs_file_f = valid_file!("/Letter.txt");
+    let abs_dir_b = valid_dir!("\\user");
+    let abs_dir_f = valid_dir!("/user");
+
+    let disk_abs_file_b = valid_file!("C:\\Letter.txt");
+    let disk_abs_file_f = valid_file!("C:/Letter.txt");
+    let disk_abs_dir_b = valid_dir!("C:\\user");
+    let disk_abs_dir_f = valid_dir!("C:/user");
+
+    let unc_disk_file = valid_file!("\\\\?\\C:\\Letter.txt\\");
+    let unc_disk_dir = valid_dir!("\\\\?\\C:\\user\\");
+
+    let cur_file_b = valid_file!(".\\cur.bin");
+    let cur_file_f = valid_file!("./cur.bin");
+    let cur_dir_b = valid_dir!(".\\cur");
+    let cur_dir_f = valid_dir!("./cur");
+
+    let parent_file_b = valid_file!("..\\parent.bin");
+    let parent_file_f = valid_file!("../parent.bin");
+    let parent_dir_b = valid_dir!("..\\parent");
+    let parent_dir_f = valid_dir!("../parent");
+
+    let hidden_dir = valid_dir!(".hidden");
+    let hidden_file = valid_file!(".hidden.bin");
+
     let here_dir = valid_dir!("dir");
     let here_file = valid_file!("file.bin");
 
     let created_file = create_file!("file.bin"!);
-    let created_new_file = create_new_file!("file.bin"!);
+    let created_new_file = create_new_file!("new_file.bin"!);
     // let invalid_new_file = create_new!("Cargo.toml"!);
     let opened_file = open_file!("Cargo.toml"!);
     // let invalid_opened_file = open!("file.bin"!);
     let read_file = read_file!("Cargo.toml"!);
     // let invalid_read_file = read!("file.bin"!);
-    let writen_file = write_file!("file.bin"!, "some text to write");
+    let writen_file = write_file!("written_file.bin"!, "some text to write");
     let created_dir = create_dir!("dir"!);
     // let invalid_created_dir = create_dir!("src"!);
-    let created_dir_all = create_dir_all!("/some/dir/tree"!);
+    let created_dir_all = create_dir_all!("some/dir/tree"!);
+
+    println!("{}", String::from_utf8_lossy(read_file));
 }
